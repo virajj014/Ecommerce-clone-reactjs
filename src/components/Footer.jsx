fefbca81b010bc73c9e94d415245848598ca2dfd
Viraj Jain <harshal.jain.csai.20@ggits.net>
2025-02-17 10:21:10 +0530

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import "./Footer.css";
+import logo from '../assets/logo.png'
+import { CiGlobe } from "react-icons/ci";
+import FooterLinks from "./FooterLinks";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="back-to-top">
+                <a href="#top">Back to top</a>
+            </div>
+            <div className="footer-content">
+                <div className="footer-section">
+                    <h3>Get to Know Us</h3>
+                    <ul>
+                        <li><a href="#">About Amazon</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Press Releases</a></li>
+                        <li><a href="#">Amazon Science</a></li>
+                    </ul>
+                </div>
+                <div className="footer-section">
+                    <h3>Connect with Us</h3>
+                    <ul>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Instagram</a></li>
+                    </ul>
+                </div>
+                <div className="footer-section">
+                    <h3>Make Money with Us</h3>
+                    <ul>
+                        <li><a href="#">Sell on Amazon</a></li>
+                        <li><a href="#">Sell under Amazon Accelerator</a></li>
+                        <li><a href="#">Protect and Build Your Brand</a></li>
+                        <li><a href="#">Amazon Global Selling</a></li>
+                        <li><a href="#">Supply to Amazon</a></li>
+                        <li><a href="#">Become an Affiliate</a></li>
+                        <li><a href="#">Fulfillment by Amazon</a></li>
+                        <li><a href="#">Advertise Your Products</a></li>
+                        <li><a href="#">Amazon Pay on Merchants</a></li>
+                    </ul>
+                </div>
+                <div className="footer-section">
+                    <h3>Let Us Help You</h3>
+                    <ul>
+                        <li><a href="#">Your Account</a></li>
+                        <li><a href="#">Returns Centre</a></li>
+                        <li><a href="#">Recalls and Product Safety Alerts</a></li>
+                        <li><a href="#">100% Purchase Protection</a></li>
+                        <li><a href="#">Amazon App Download</a></li>
+                        <li><a href="#">Help</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <img src={logo} onClick={() => navigate('/')} className="logo" />
+
+                <div className="footer-buttons">
+                    <button className="language-btn"><CiGlobe />
+                        English</button>
+                    <button className="country-btn">
+                        <img src='https://flagcdn.com/w320/in.png' alt='' className='flag-icon'
+                        />
+                        India
+                    </button>
+                </div>
+            </div>
+            <FooterLinks/>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ToastContainer } from 'react-toastify'
+import Footer from './components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Footer/>
     <ToastContainer />
   </StrictMode>,
 )
